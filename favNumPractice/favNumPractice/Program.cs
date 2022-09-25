f342ccd891375aa7f7cc75e9bea8cf2d725cb357
calebtripp <calebtripp@outlook.com>
2022-09-24 21:05:07 -0400

    guess favorite number game complete

---
+++ favNumPractice/favNumPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Welcome to the Guess My Favorite Number Game!\nTake a guess then press enter to see if you got it right!\n ");
+var favNum = 2;
+var guess = Convert.ToInt32(Console.ReadLine());
+
+
+if (guess == favNum)
+{
+    Console.WriteLine("Congrats, you guessed it!");
+}
+
+while (guess != favNum)
+{
+    Console.WriteLine("Not quite, take another guess");
+    var guessAgain = Convert.ToInt32(Console.ReadLine());
+
+    if (guessAgain == favNum)
+    {
+        Console.WriteLine("Congrats, you guessed it!");
+        break;
+    }
+}
